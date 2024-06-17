42dc27431a6919dc35966d8fc4625a8ba86a476a
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-17 07:05:31 +0000

    EJS Intrapolation syntax added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'/views'))
 
 // app.get('/',(req,res)=>{
 //     res.render('views/home.ejs')
